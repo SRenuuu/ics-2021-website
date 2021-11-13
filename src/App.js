2382d2d427ef41c8dddb8d66cd3896bfca221bfa
Sandul Renuja <sandulrenuja@gmail.com>
2021-11-13 11:36:55 +0530

    Implement lazy loading for all components

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import AOS from 'aos';
 import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
 import './assets/scss/styles-main.scss';
-import loadable from '@loadable/component'
+import LazyLoad from 'react-lazyload';
 
-const About = loadable(() => import("./components/About"))
-const Awards = loadable(() => import("./components/Awards"))
-const Sponsors = loadable(() => import("./components/Sponsors"))
-const FAQ = loadable(() => import("./components/Faq"))
-const Gallery = loadable(() => import("./components/Gallery"))
-const Timeline = loadable(() => import("./components/TimelineMain"))
-const ContactUs = loadable(() => import("./components/ContactUs"))
-const Footer = loadable(() => import("./components/Footer"))
+import About from "./components/About";
+import Awards from "./components/Awards";
+import FAQ from "./components/Faq";
+import Sponsors from './components/Sponsors';
+
+import Footer from "./components/Footer"
+import Gallery from "./components/Gallery"
+import Timeline from "./components/TimelineMain";
+import ContactUs from "./components/ContactUs";
+
+// const About = loadable(() => import("./components/About"))
+// const Awards = loadable(() => import("./components/Awards"))
+// const Sponsors = loadable(() => import("./components/Sponsors"))
+// const FAQ = loadable(() => import("./components/Faq"))
+// const Gallery = loadable(() => import("./components/Gallery"))
+// const Timeline = loadable(() => import("./components/TimelineMain"))
+// const ContactUs = loadable(() => import("./components/ContactUs"))
+// const Footer = loadable(() => import("./components/Footer"))
 
 const App = () => {
     useEffect(() => {
@@ -31,12 +41,24 @@ const App = () => {
             <NavBar/>
             <HeroSection/>
             <About/>
-            <Timeline/>
-            <Awards/>
-            <Sponsors/>
-            <Gallery/>
-            <FAQ/>
-            <ContactUs/>
+            <LazyLoad once offset={200}>
+                <Timeline/>
+            </LazyLoad>
+            <LazyLoad once offset={200}>
+                <Awards/>
+            </LazyLoad>
+            <LazyLoad once offset={200}>
+                <Sponsors/>
+            </LazyLoad>
+            <LazyLoad once offset={200}>
+                <Gallery/>
+            </LazyLoad>
+            <LazyLoad once offset={200}>
+                <FAQ/>
+            </LazyLoad>
+            <LazyLoad once offset={200}>
+                <ContactUs/>
+            </LazyLoad>
             <Footer/>
         </main>
     );
